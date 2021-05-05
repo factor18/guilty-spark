3f4d4d90fc25a6d61ef7f025caeba4cc2fcf89fe
Sarat Chandra Balla <sarat1669@gmail.com>
2021-05-05 20:48:27 +0530

    add support for brew --cask

--- installers/prerequisites.rb
+++ installers/prerequisites.rb
@@ -1,28 +1,33 @@
 module Installers
   class Prerequisites
     $brew_list = [
-      {tap: "jq", command: "jq"},
-      {tap: "fzf", command: "fzf"},
-      {tap: "bat", command: "bat"},
-      {tap: "tree", command: "tree"},
-      {tap: "tldr", command: "tldr"},
-      {tap: "tmux", command: "tmux"},
-      {tap: "wget", command: "wget"},
-      {tap: "httpie", command: "http"},
-      {tap: "thefuck", command: "fuck"},
-      {tap: "elixir", command: "elixir"},
-      {tap: "trash-cli", command: "trash"},
+      {formula: "jq", command: "jq"},
+      {formula: "fzf", command: "fzf"},
+      {formula: "bat", command: "bat"},
+      {formula: "tree", command: "tree"},
+      {formula: "tldr", command: "tldr"},
+      {formula: "tmux", command: "tmux"},
+      {formula: "wget", command: "wget"},
+      {formula: "httpie", command: "http"},
+      {formula: "thefuck", command: "fuck"},
+      {formula: "elixir", command: "elixir"},
+      {formula: "thrift", command: "thrift"},
+      {formula: "trash-cli", command: "trash"},
+      {formula: "ngrok", command: "ngrok", cask: true},
+      {tap: "gigalixir/brew", formula: "gigalixir", command: "gigalixir"},
     ]
 
     def ensure_installation
       $log.info "#{$identity}: Installing prerequisites"
       $brew_list.each do |item|
         next if(system("which #{item[:command]} > /dev/null"))
-        if !system("brew install #{item[:tap]}") || !system("which #{item[:command]} > /dev/null")
-          $log.info "#{$identity}: Failed to install #{item[:tap]}. Try running #{$PROGRAM_NAME} again"
+        print "."
+        if !item[:tap].nil? && !system("brew tap #{item[:cask] ? "--cask" : ""} #{item[:tap]} > /dev/null") || !system("brew install #{item[:formula]} > /dev/null") || !system("which #{item[:command]} > /dev/null")
+          $log.info "#{$identity}: Failed to install #{item[:formula]}. Try running #{$PROGRAM_NAME} again"
           exit(false)
         end
       end
+      puts ""
       system("/usr/local/opt/fzf/install --all > /dev/null")
       $log.info "#{$identity}: Prerequisites are installed"
     end
